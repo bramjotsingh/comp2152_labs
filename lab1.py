9f2cc1c20461771c675a442d735704bf870c1b7e
bramjotsingh <101511990@georgebrown.ca>
2025-02-02 19:22:51 -0500

    All Labs

---
+++ lab1.py
@@ -0,0 +1,21 @@
+
+numbers = [1, 4, 7, 9]
+print("Array variable:", numbers)
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a - ((b ** c) // d) + (a % c))
+print("Value of e:", e)
+
+temperature = 32.6
+
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Please enter your age: "))
+
+userAge += 22
+
+print("Now showing the shop items filtered by age:", userAge)
